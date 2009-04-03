4e04f741f0cadec55a26595dbb7ce8b182f74d00
Till Klampaeckel <till@php.net>
2009-04-03 17:05:43 +0200

    forcing accept header: xml, random fixes, docs, class/file header

--- Services/DoingText.php
+++ Services/DoingText.php
@@ -1,4 +1,45 @@
 <?php
+/** 
+ * +-----------------------------------------------------------------------+
+ * | Copyright (c) 2009, Till Klampaeckel                                  |
+ * | All rights reserved.                                                  |
+ * |                                                                       |
+ * | Redistribution and use in source and binary forms, with or without    |
+ * | modification, are permitted provided that the following conditions    |
+ * | are met:                                                              |
+ * |                                                                       |
+ * | o Redistributions of source code must retain the above copyright      |
+ * |   notice, this list of conditions and the following disclaimer.       |
+ * | o Redistributions in binary form must reproduce the above copyright   |
+ * |   notice, this list of conditions and the following disclaimer in the |
+ * |   documentation and/or other materials provided with the distribution.|
+ * | o The names of the authors may not be used to endorse or promote      |
+ * |   products derived from this software without specific prior written  |
+ * |   permission.                                                         |
+ * |                                                                       |
+ * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
+ * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
+ * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
+ * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
+ * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
+ * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
+ * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
+ * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
+ * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
+ * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
+ * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
+ * |                                                                       |
+ * +-----------------------------------------------------------------------+
+ *
+ * PHP Version 5
+ *
+ * @category Services
+ * @package  Services_DoingText
+ * @author   Till Klampaeckel <till@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  GIT: ID
+ * @link     http://github.com/till/services_doingtext/tree/master
+ */
 
 /**
  * Services_DoingText_Exception
@@ -6,13 +47,17 @@
 require_once 'Services/DoingText/Exception.php';
 
 /**
- * @category Services
+ * Services_DoingText
  *
- * @author Till Klampaeckel <till@php.net>
- * @license New BSD License
- * @todo Services_DoingText::setClient()
- * @todo Services_DoingText::setAuth()
- * @todo Services_DoingText::setEndpoint()
+ * @category Services
+ * @package  Services_DoingText
+ * @author   Till Klampaeckel <till@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  Release: @package_version@
+ * @link     http://github.com/till/services_doingtext/tree/master
+ * @todo     Services_DoingText::setClient()
+ * @todo     Services_DoingText::setAuth()
+ * @todo     Services_DoingText::setEndpoint()
  */
 class Services_DoingText
 {
@@ -20,6 +65,12 @@ class Services_DoingText
     protected $httpClient;
     protected $password;
     protected $username;
+    
+    /**
+     * Error/Status codes, conform to HTTP.
+     * @access global
+     */
+    const ERR_PRECONDITION = 412;
 
     /**
      * __construct
@@ -40,13 +91,20 @@ class Services_DoingText
                 include_once 'HTTP/Request2.php';
             }
             if (!class_exists('HTTP_Request2')) {
-                throw new Services_DoingText_Exception('Could not load HTTP_Request2.', 500);
+                throw new Services_DoingText_Exception(
+                    'Could not load HTTP_Request2.',
+                    self::ERR_PRECONDITION
+                );
             }
             $this->httpClient = new HTTP_Request2();
+            $this->httpClient->setAuth($this->username, $this->password);
         } else {
             $this->httpClient = $httpClient;
+            $auth = $this->httpClient->getAuth();
+            if (!isset($auth['username']) || empty($auth['username'])) {
+                $this->httpClient->setAuth($this->username, $this->password);
+            }
         }
-        $this->httpClient->setAuth($username, $password);
     }
 
     /**
@@ -60,21 +118,20 @@ class Services_DoingText
      */
     public function add($content, $title = null, $permaLink = null)
     {
-        $data               = array();
-        $data['discussion'] = array();
+        $data = array();
 
         if ($title !== null) {
-            $data['discussion']['title'] = $title;
+            $data['title'] = $title;
         }
         if ($permaLink === null) {
-            $data['discussion']['permaLink'] = $permaLink;
+            $data['permalink'] = $permaLink;
         }
-        $data['discussion']['content'] = $content;
+        $data['content'] = $content;
         
-        $this->httpClient->addPostParameter($data);
-        
+        $this->httpClient->addPostParameter(array('discussion' => $data));
+
         $response = $this->makeRequest('discussions.xml', 'POST');
-        
+
         $obj = $this->parseResponse($response);
         return $obj;
     }
@@ -111,26 +168,47 @@ class Services_DoingText
      */
     protected function makeRequest($url, $method = 'GET')
     {
+        $method = strtoupper($method);
+
+        switch($method) {
+        case 'GET':
+        case 'POST':
+        case 'PUT':
+            break;
+        default:
+            throw new Services_DoingText_Exception(
+                "Unsupported method {$method} used.",
+                self::ERR_PRECONDITION
+            );
+        }
         $uri = $this->endpoint . $url;
         
+        $this->httpClient->setHeader('Accept', 'xml');
         $this->httpClient->setUrl($uri);
 
         $this->httpClient->setMethod($method);
         $response = $this->httpClient->send();
+        $status   = $response->getStatus();
+        $body     = $response->getBody();
         
-        if (!in_array($response->getStatus(), array(200, 304))) {
-            switch ($response->getStatus()) {
+        if (!in_array($status, array(200, 304))) {
+            switch ($status) {
             case 401:
                 $msg = 'Wrong username and/or password';
                 break;
+            case 422:
+                $msg = "Provided input parameters are invalid.";
+                var_dump($body);
+                break;
             default:
-                $msg = "An error occured: {$response->getStatus()}";
+                $msg  = "An error occured: {$status}\n";
+                $msg .= "Message: {$body}";
                 break;
             }
-            throw new Services_DoingText_Exception($msg, $response->getStatus());
+            throw new Services_DoingText_Exception($msg, $status);
         }
         
-        return $response->getBody();
+        return $body;
     }
 
     /**
