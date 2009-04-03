2d9aa4ba3d97486dc22916dad8d8a532538318d3
Till Klampaeckel <till@php.net>
2009-04-03 17:05:03 +0200

    added class/file header, PEAR_Exception

--- Services/DoingText/Exception.php
+++ Services/DoingText/Exception.php
@@ -1,4 +1,62 @@
 <?php
-class Services_DoingText_Exception extends Exception
+/** 
+ * +-----------------------------------------------------------------------+
+ * | Copyright (c) 2009, Till Klampaeckel                                  |
+ * | All rights reserved.                                                  |
+ * |                                                                       |
+ * | Redistribution and use in source and binary forms, with or without    |
+ * | modification, are permitted provided that the following conditions    |
+ * | are met:                                                              |
+ * |                                                                       |
+ * | o Redistributions of source code must retain the above copyright      |
+ * |   notice, this list of conditions and the following disclaimer.       |
+ * | o Redistributions in binary form must reproduce the above copyright   |
+ * |   notice, this list of conditions and the following disclaimer in the |
+ * |   documentation and/or other materials provided with the distribution.|
+ * | o The names of the authors may not be used to endorse or promote      |
+ * |   products derived from this software without specific prior written  |
+ * |   permission.                                                         |
+ * |                                                                       |
+ * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
+ * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
+ * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
+ * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
+ * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
+ * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
+ * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
+ * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
+ * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
+ * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
+ * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
+ * |                                                                       |
+ * +-----------------------------------------------------------------------+
+ *
+ * PHP Version 5
+ *
+ * @category Services
+ * @package  Services_DoingText
+ * @author   Till Klampaeckel <till@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  GIT: ID
+ * @link     http://github.com/till/services_doingtext/tree/master
+ */
+
+/**
+ * PEAR_Exception
+ * @ignore
+ */
+require_once 'PEAR/Exception.php';
+
+/**
+ * Services_DoingText
+ *
+ * @category Services
+ * @package  Services_DoingText
+ * @author   Till Klampaeckel <till@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  Release: @package_version@
+ * @link     http://github.com/till/services_doingtext/tree/master
+ */
+class Services_DoingText_Exception extends PEAR_Exception
 {
 }
